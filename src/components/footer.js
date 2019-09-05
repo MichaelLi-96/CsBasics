6cbc6641ebbe333303991fb40c1870549c3843e4
Michael <michaelli.sw.eng@gmail.com>
2019-09-05 16:04:44 -0700

    Added daily inspirational quote of the day

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { FaRegCopyright } from "react-icons/fa";
 import '../assets/footer.css';
 
 class Footer extends Component {
-  render() {
-    return(
-		<footer>
-			<div>
-				<text id="FooterText"><FaRegCopyright id="CopyrightIcon" /> Michael Li</text>
-			</div>
-		</footer>
-    );
-  }
+  	render() {
+	    return(
+			<footer>
+				<div>
+					<div id="FooterText"><FaRegCopyright id="CopyrightIcon" /> Michael Li</div>
+				</div>
+			</footer>
+	    );
+  	}
 }
 
 export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -5,48 +5,48 @@ import logo from "../assets/logo.png";
 import "../assets/header.css"
 
 class Header extends Component {
-  render() {
-    return(
-    	<header id="header">
-			<Navbar id="navbar" collapseOnSelect expand="lg" variant="dark" activeKey="/home" sticky="top">
-				<Link to="/">
-				 	<Navbar.Brand style={styles.logo}>
-						<img
-							alt="csBasics"
-							src={logo}
-							className="d-inline-block align-top"
-						/>
-					</Navbar.Brand>
-				</Link>
-			  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			  <Navbar.Collapse id="responsive-navbar-nav">
-			    <Nav>	
-					<ul>
-						<li>
-							<Link to="/DataTypes">Data Types</Link>
-						</li>
-						<li>
-							<Link to="/ObjectOrientedProgramming">Object Oriented Programming</Link>
-						</li>
-						<li>
-							<Link to="/DataStructures">Data Structures</Link>
-						</li>
-						<li>
-							<Link to="/Algorithms">Algorithms</Link>
-						</li>
-						<li>
-							<Link to="/Sorting">Sorting</Link>
-						</li>
-						<li>
-							<Link to="/Searching">Searching</Link>
-						</li>
-					</ul>
-				</Nav>
-			  </Navbar.Collapse>
-			</Navbar>
-		</header>
-    );
-  }
+  	render() {
+	    return(
+	    	<header id="header">
+				<Navbar id="navbar" collapseOnSelect expand="lg" variant="dark" sticky="top">
+					<Link to="/">
+					 	<Navbar.Brand style={styles.logo}>
+							<img
+								alt="csBasics"
+								src={logo}
+								className="d-inline-block align-top"
+							/>
+						</Navbar.Brand>
+					</Link>
+				  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				  <Navbar.Collapse id="responsive-navbar-nav">
+				    <Nav>	
+						<ul>
+							<li>
+								<Link to="/DataTypes">Data Types</Link>
+							</li>
+							<li>
+								<Link to="/ObjectOrientedProgramming">Object Oriented Programming</Link>
+							</li>
+							<li>
+								<Link to="/DataStructures">Data Structures</Link>
+							</li>
+							<li>
+								<Link to="/Algorithms">Algorithms</Link>
+							</li>
+							<li>
+								<Link to="/Sorting">Sorting</Link>
+							</li>
+							<li>
+								<Link to="/Searching">Searching</Link>
+							</li>
+						</ul>
+					</Nav>
+				  </Navbar.Collapse>
+				</Navbar>
+			</header>
+	    );
+  	}
 }
 
 let styles = {
