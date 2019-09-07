7c7d313e9ae1dbcab5a36bc03bc8f21d50b227be
Michael <michaelli.sw.eng@gmail.com>
2019-09-07 15:42:14 -0700

    Added syntax highlighting

--- src/components/pages/oop.js
+++ src/components/pages/oop.js
@@ -1,9 +1,73 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { androidstudio } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import "../../assets/pages.css";
 import { FaStar } from "react-icons/fa";
 
 class Oop extends Component {
   	render() {
+  		const classExample =
+`//A car class that stores the model, color, and milesDriven
+public class Car {
+	//variables
+	private String model;
+	private String color;
+	private int milesDriven;
+
+	//constructor 
+	//The constructor is where you set up all your class variables
+	//The constructor name is always the same as your class name
+	//This is what happens when you create a new Car object
+	publc Car(String model, String color) {
+		this.model = model;
+		this.color = color;
+		milesDriven = 0;
+	}
+	//methods 
+	//Methods can be mutators or accessors
+	//Accessor methods only retrieve objects data
+	//Mutator methods change the objects data
+
+	//accessors (getters)
+	public String getModel() {
+		return model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getMilesDriven() {
+		return milesDriven;
+	}
+
+	//mutators (setters)
+	public void setToNewColor(String newColor) {
+		color = newColor;
+	}
+	public void drive(int milesDrove) {
+		milesDriven = milesDriven + milesDrove;
+	}
+}`;
+		const objectExample =
+`//A car tester class where we can create car objects and test its methods
+public class CarTester {
+    //This is our main function where our code will run
+    public static void main(String[] args) {
+    	//This creates a new car object with the model "Truck"
+    	Car myCar = new Car("Truck", "Blue");
+
+    	//Store the color of the myCar object in a String called color
+    	String color = myCar.getColor();
+    	This will print out "Blue"
+    	System.out.println(color);
+
+    	//Set the color of the myCar object to white
+    	myCar.setToNewColor("White");
+    	color = myCar.getColor();
+    	//This will now print out "White"
+    	System.out.println(color);
+    }
+}`;
 	    return(
 	    	 <div className="flex-container">
 				<div className="title">Object-Oriented Programming</div>
@@ -12,7 +76,7 @@ class Oop extends Component {
 					These objects are then used to communicate with each other to ultimately solve a problem.
 				</div>
 				<div className="content">
-					<div className="subtitle">What are classes and objects?</div>
+					<div className="subtitle-left">What are classes and objects?</div>
 					<div className="text-left">
 						<FaStar className="starIcon"/>
 						A class describes a set of objects with the same behavior, containing variables and methods common to all objects of that kind. 
@@ -23,22 +87,46 @@ class Oop extends Component {
 						A object is a program entity that the programmer can manipulate by calling its methods. 
 						<mark> An object is an instance of a class.</mark>
 					</div>
-					<div className="subtitle">Four Main OOP Principles</div>
-					<div className="subsubtitle">Encapsulation</div>
+					<Container fluid>
+						<Row>
+							<Col className="subsubtitle-center">
+								Creating a car class
+							</Col>
+							<Col className="subsubtitle-center">
+								Creating an car object using the car class
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<SyntaxHighlighter language="java" showLineNumbers style={androidstudio} className="code-text">
+									{classExample}
+								</SyntaxHighlighter>	
+							</Col>
+							<Col>					
+								<SyntaxHighlighter language="java" showLineNumbers style={androidstudio} className="code-text">
+									{objectExample}
+								</SyntaxHighlighter>	
+							</Col>
+						</Row>
+						<br />
+						<br />
+					</Container>
+					<div className="subtitle-left">Four Main OOP Principles</div>
+					<div className="subsubtitle-left">Encapsulation</div>
 					<div className="text-left">
 						Encapsulation is the practice of keeping fields(instance variables) private, only providing methods to access and modify them.
 						An objects instance variables store the data required for executing methods. This means nobody else can modify the state of
 						the object without using its methods unless explicitly allowed. This helps programmers use a class without knowing its implementation 
 						because the implementation details are hidden and only methods for data access are provided for use.
 					</div>
-					<div className="subsubtitle">Abstraction</div>
+					<div className="subsubtitle-left">Abstraction</div>
 					<div className="text-left">
 						Applying abstraction means that each object should only expose a high-level mechanism for using it. Programmers do not need to 
 						know the underlying code and mechanisms in order to use the object. An example of this would be a man driving a car. The man
 						only knows that pressing the accelerator speeds up the car and pressing the brakes will stop the car. Even though the man does not know 
 						the inner mechanisms of the car and exactly how the accelerator and brake is implemented, he is still able to drive the car.
 					</div>
-					<div className="subsubtitle">Inheritance</div>
+					<div className="subsubtitle-left">Inheritance</div>
 					<div className="text-left">
 						Inheritance allows programmers to create new classes that share some or all of the attributes of existing classes. This lets 
 						a new class adopt the properties of another. The inheriting class is called the subclass or child class. The original class is 
@@ -48,7 +136,7 @@ class Oop extends Component {
 						and inherit some of the attributes of car.
 					</div>
 
-					<div className="subsubtitle">Polymorphism</div>
+					<div className="subsubtitle-left">Polymorphism</div>
 					<div className="text-left">
 						The definition of polymorphism is the condition of occuring in different forms. In programming, polymorphism lets programmers
 						use the same word to mean different things in different contexts by utilizing method overloading and method overriding. To show an example
