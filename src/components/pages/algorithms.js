a003d67b4f046cc01d43d45d5eabe54067dfafa0
Michael <michaelli.sw.eng@gmail.com>
2019-10-04 10:41:40 -0700

    Testing mlab

--- src/components/pages/algorithms.js
+++ src/components/pages/algorithms.js
@@ -122,7 +122,7 @@ class Algorithms extends Component {
 
 	componentDidMount() {
 	  window.scrollTo(0, 0);
-	  axios.get(process.env.baseURL || 'mongodb://127.0.0.1:27017/algorithms')
+	  axios.get('https://csbasics.herokuapp.com/')
 	  	.then(response => {
 	  		this.setState({ algorithms: response.data });
 	  	})
