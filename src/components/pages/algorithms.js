5b84cf8c675345d7932183e5c886348657564ce2
Michael <michaelli.sw.eng@gmail.com>
2019-10-03 17:40:07 -0700

    testing axios

--- src/components/pages/algorithms.js
+++ src/components/pages/algorithms.js
@@ -122,7 +122,7 @@ class Algorithms extends Component {
 
 	componentDidMount() {
 	  window.scrollTo(0, 0);
-	  axios.get('http://localhost:4000/algorithms')
+	  axios.get(process.env.MONGODB_URI || 'http://localhost:4000/algorithms')
 	  	.then(response => {
 	  		this.setState({ algorithms: response.data });
 	  	})
